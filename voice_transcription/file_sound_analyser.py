08d46421ef761725cd1acd6b5da53af93a06b096
Sandeep Kumar Mishra <sk2011mishra@gmail.com>
2022-12-03 21:23:56 +0530

    sound analyser

--- voice_transcription/file_sound_analyser.py
+++ voice_transcription/file_sound_analyser.py
@@ -2,22 +2,32 @@ import numpy as np
 import pyaudio as pa 
 import struct 
 import matplotlib.pyplot as plt 
+import os
+import wave
 
-CHUNK = 1024 * 2
+#CHUNK = 1024 * 2
+CHUNK = 1024
 FORMAT = pa.paInt16
 CHANNELS = 1
 RATE = 44100 # in Hz
-
+file_path = '../data/audio/aeka_audio/'
+files = os.listdir(file_path)
+file = 'AEKA0_tcp-27_11_2022-16-08-20.wav'
 p = pa.PyAudio()
-
+#for file in files:
+wf = wave.open(file_path + file, 'rb')
 stream = p.open(
-    format = FORMAT,
-    channels = CHANNELS,
-    rate = RATE,
-    input=True,
-    output=True,
-    frames_per_buffer=CHUNK
-)
+    format =p.get_format_from_width(wf.getsampwidth()),
+    channels = wf.getnchannels(),
+    rate = wf.getframerate(),
+    output = True)
+        #format = FORMAT,
+        #channels = CHANNELS,
+        #rate = RATE,
+        #input=True,
+        #output=True,
+        #frames_per_buffer=CHUNK
+    #)
 
 
 
